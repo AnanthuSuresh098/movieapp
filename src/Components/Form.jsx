17dec7260fdcc6a502dd466f14191c2b6ebd0083
AnanthuSuresh098 <93374262+AnanthuSuresh098@users.noreply.github.com>
2022-06-30 02:07:49 +0530

    components commit

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,3 +1,99 @@
+import "./Form.css"
+import { useState } from "react";
+
 export const Form=()=>{
-    return <h1>Form</h1>
+
+const [name, setName] = useState("");
+
+const [email, setEmail] = useState("");
+
+const handleNameInput=(e)=>{
+    setName(e.target.value)
+}
+
+const handleEmailInput = (e) => {
+  setEmail(e.target.value);
+};
+
+
+const handleClick=()=>{
+    if(name==""){
+        alert("Please enter name")
+    }
+    if (email == "") {
+      alert("Please enter a valid email");
+    }
+}
+
+    return (
+      <div>
+        <h1>Register here !</h1>
+        <form>
+          <div class="mb-3">
+            <label for="Name" class="form-label">
+              Name
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              value={name}
+              onChange={handleNameInput}
+            ></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="exampleInputEmail1" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              value={email}
+              onChange={handleEmailInput}
+            ></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="mobile" class="form-label">
+              Mobile
+            </label>
+            <input type="number" class="form-control" id="mobile"></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="country" class="form-label">
+              Country
+            </label>
+            <input type="text" class="form-control" id="country"></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="city" class="form-label">
+              City
+            </label>
+            <input type="text" class="form-control" id="city"></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="state" class="form-label">
+              State
+            </label>
+            <input type="text" class="form-control" id="state"></input>
+          </div>
+
+          <div class="mb-3">
+            <label for="message" class="form-label">
+              Message
+            </label>
+            <input type="text" class="form-control" id="message"></input>
+          </div>
+
+          <button type="submit" class="submit-btn" onClick={handleClick}>
+            Submit
+          </button>
+        </form>
+      </div>
+    );
 }
